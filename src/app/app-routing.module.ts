cde3a17aacbff8c322066656c5623c4a76fdf7cc
Mary <mary_hbb@yahoo.com>
2023-01-25 23:05:20 +0100

    Implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExploreComponent } from './components/explore/explore.component';
+import { TweetComponent } from './components/tweet/tweet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: ExploreComponent},
+{path: 'tweet', component: TweetComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TweetComponent } from './components/tweet/tweet.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ExploreComponent } from './components/explore/explore.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TweetComponent,
-    NavbarComponent
+    NavbarComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
