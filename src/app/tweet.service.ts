517e602aa2c4e1d13ab0311b33fb4ef0012c2de1
Mary <mary_hbb@yahoo.com>
2023-02-01 23:42:48 +0100

    Implement DataService and add in Explore and create tweet

--- src/app/tweet.service.ts
+++ src/app/tweet.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Tweet } from './models/tweet';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,19 @@ import { Injectable } from '@angular/core';
 export class TweetService {
 
   constructor() { }
+
+  public tweets : Tweet [] = [
+    {
+      user : "President Biden",
+      avatar: "assets/images/biden.jpg",
+      date: "11 Jan 2023",
+      massage: "I'm building back an economy with better jobs and better pay -one that includes good-paying jobs that don’t require a four-year degree, and communities that are too often forgotten."
+    },
+    {
+      user : "President Biden",
+      avatar: "assets/images/biden.jpg",
+      date: "11 Jan 2023",
+      massage: "I'm building back an economy with better jobs and better pay -one that includes good-paying jobs that don’t require a four-year degree, and communities that are too often forgotten."
+    }
+  ];
 }
